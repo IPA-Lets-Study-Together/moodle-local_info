db9277110ef308406701c9938af4f2c0d84d8a27
Moirae <is.moirae@gmail.com>
2014-03-18 14:29:17 +0100

    Table&Image verification

--- index.php
+++ index.php
@@ -25,9 +25,10 @@ echo $OUTPUT->header();
 
 echo $OUTPUT->box(get_string('greet', 'local_info', format_string($name)));
 
-global $DB;
-
 try {
+
+	//connecting to database
+	global $DB;
 	$sql = 'SELECT id FROM mdl_book WHERE name = ?';
 	$params = array('Povijesni razvoj računala');
 
@@ -36,32 +37,50 @@ try {
 	$sql = 'SELECT content FROM mdl_book_chapters WHERE bookid = ?';
 	$params = array($result->id);
 
-	$result02 = $DB->get_records_sql_menu($sql, $params);
+	$result02 = $DB->get_records_sql_menu($sql, $params); //getting book chapter
 
 	print_r($result02);
 
-	$subject = serialize($result02); //array to string conversion??
-	$pattern = '/<img(.*)\/>/';
+	$book_chaper = serialize($result02); //converting book chapter array to string
 
-	preg_match_all($pattern, $subject, $preg_array); //getting preg_match_all
+	$pattern_image = '/<img(.*)\/>/'; //regular expression for image tag search
+	$pattern_table = '/<table(.*)\>/'; //regular expression for table tag search
 
-	print_r($preg_array);
-	// if (preg_match($pattern, $subject)) {
-	// 	echo "img was found";
-	// } else {
-	// 	echo "no image in file";
-	// }
+	preg_match_all($pattern_image, $book_chaper, $preg_image); //getting preg_match_all for image
+	preg_match_all($pattern_table, $book_chaper, $preg_table);
 
-	$preg_array_indices = count($preg_array);
+	print_r($preg_image);
+	print_r($preg_table);
 
-	echo $preg_array_indices;
+	if (!empty($preg_image)) {
 
-	while (in_array('alt="', $preg_array)) {
-		$cnt++;
-	 	echo "there are some image alts";
+		$preg_image_indices = count($preg_image); //counting image indices
+		
+		$alt_search = array_search('alt="', $preg_image); //searching for image alt tag
+
+		$cnt_alt = count($alt_search);
+
+		if ($cnt_alt < $preg_image_indices) {
+			echo "Slike nisu validirane";
+
+			$record = new stdClass();
+			
+		}
 	}
 
-	echo $cnt;
+	if (!empty($preg_table)) {
+
+		$preg_table_indices = count($preg_table); //counting table indices
+		$summary_search = array_search('summary="', $preg_table); //searching for table summary tag
+	}
+	
+
+	
+	
+
+	
+
+	/*echo $cnt;*/
 
 } catch(Exception $e) {
 	echo "fail";
